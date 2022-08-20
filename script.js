cf4c91bbf3383942a585b8af993db1e9fb9b0340
Jonas Tiago <jonastiago95@gmail.com>
2022-08-20 18:49:13 -0300

    virada de cartas e correção

--- script.js
+++ script.js
@@ -34,3 +34,50 @@ function chamarCartas(numeroDesejado) {
             entregue++;  
     }
 }
+
+/** Sortear as cartas*/
+
+function sortear(qtdCartas) {
+    const todasAsCartas = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+    let tipos = qtdCartas
+    let escolhas = todasAsCartas.splice(0, tipos)
+    
+    return escolhas
+}
+
+function embaralhar() {
+    return Math.random() - 0.5;
+
+}
+
+
+/* viranddo as cartas */
+
+let primeiraCarta, segundaCarta;
+let selecionadas = []
+let jogadas = 0;
+
+function selecionarCarta(carta) {
+    carta.classList.add("giro");
+
+   if (!primeiraCarta) {
+        selecionadas.push(carta) 
+        primeiraCarta = carta.dataset.carta;
+        console.log('primeira')
+    }else {
+        selecionadas.push(carta)
+        segundaCarta = carta.dataset.carta;
+        console.log('segunda')
+    }
+
+    if(selecionadas.length == 2){
+        setTimeout(comparandoCartas, 800)
+     }
+     else if(selecionadas.length > 2){
+        alert("Apenas duas cartas por vez")
+        selecionadas.forEach( (carta)=> {
+            carta.classList.remove('giro')
+        })
+        selecionadas.splice(0, (selecionadas.length))
+     }
+}
