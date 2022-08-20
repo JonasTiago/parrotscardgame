7a3159a669b3fafe003520ed3249c2636dc232f8
Jonas Tiago <jonastiago95@gmail.com>
2022-08-20 18:44:30 -0300

    Cartas embaralhadas

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const qtdCartas = prompt("quantas cartas")
+/*pedindo cartas*/
 
-if(qtdCartas > 0){
+let qtdCartas = prompt("Digite a quantidade de quartas que deseja (pares de 4 a 14):")
+
+let contador;
+while (contador !== 0) {
+    if (qtdCartas >= 4 && qtdCartas <= 14 && (qtdCartas % 2 === 0)) {
+        contador = 0;
+        chamarCartas(qtdCartas)
+
+    } else {
+        qtdCartas = prompt("Digite a quantidade de quartas que deseja (pares de 4 a 14):")
+    }
+}
+
+/*Distribuido as cartas */
+
+function chamarCartas(numeroDesejado) {
+
     const jogo = document.querySelector('.game-zone')
-    let contador = 0;
-    while(qtdCartas > contador){
-        console.log(jogo.innerHTML)
-        jogo.innerHTML += `<li></li>`
-        contador++;
+    
+    const cartasEmbaralhadas = sortear(numeroDesejado)
+    cartasEmbaralhadas.sort(embaralhar)
+    
+    let entregue = 0;
+    while (numeroDesejado > entregue) {
+            jogo.innerHTML += `
+                <div id="carta" class="carta" onclick="selecionarCarta(this)" data-carta="${cartasEmbaralhadas[entregue]}">
+                    <div class="face" id="front"><img src="img/${cartasEmbaralhadas[entregue]}.gif" alt=""></div>
+                    <div class="face" id="back"></div>
+                </div>
+                `
+
+            entregue++;  
     }
 }
